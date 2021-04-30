888e6afdf366df39fe836c07c158965cc26ce93d
henrymcbean <henry_mcbean@hotmail.com>
2021-04-30 22:25:24 +0100

    Bug fix missing s of params.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/api/v1/bootcamps', (req, res) => {
 });
 
 app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Get bootcamp ${req.param.id}` });
+  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
 });
 
 app.post('/api/v1/bootcamps', (req, res) => {
@@ -21,13 +21,13 @@ app.post('/api/v1/bootcamps', (req, res) => {
 app.put('/api/v1/bootcamps/:id', (req, res) => {
   res
     .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.param.id}` });
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
 });
 
 app.delete('/api/v1/bootcamps/:id', (req, res) => {
   res
     .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.param.id}` });
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
 });
 
 const PORT = process.env.PORT || 5000;
