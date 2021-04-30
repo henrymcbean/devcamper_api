8fa70a7882fcfca909b59ac3a4f5ec110ddc67ff
henrymcbean <henry_mcbean@hotmail.com>
2021-04-30 22:22:07 +0100

    3. Creating Routes  Responses In Express.

--- server.js
+++ server.js
@@ -6,8 +6,34 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Get bootcamp ${req.param.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamps' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.param.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.param.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode and listening on port ${PORT}`);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode and listening on port ${PORT}`
+  );
 });
