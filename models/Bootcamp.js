99efa83ad3037505f86e9b0b2a0781c49a29bcbd
henrymcbean <henry_mcbean@hotmail.com>
2021-05-02 21:22:04 +0100

    3. Advanced Filtering, bug fix in Bootcamp model.

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -114,10 +114,10 @@ BootcampSchema.pre('save', async function(next) {
     coordinates: [loc[0].longitude, loc[0].latitude],
     formattedAddress: loc[0].formattedAddress,
     street: loc[0].streetName,
-    street: loc[0].city,
-    street: loc[0].stateCode,
-    street: loc[0].zipcode,
-    street: loc[0].countryCode,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
   };
 
   // Do not save address in DB
