b02e037b76c58f6ca99bbc63f9ec7292837786b0
henrymcbean <henry_mcbean@hotmail.com>
2021-05-02 19:33:05 +0100

    7. GeoJSON Location  Geocoder Hook - MapQuest API.

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const { default: slugify } = require('slugify');
-const slugif = require('slugify');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -103,6 +103,26 @@ const BootcampSchema = new mongoose.Schema({
 // Create bootcamp slug from name
 BootcampSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode & create location field
+BootcampSchema.pre('save', async function(next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    street: loc[0].city,
+    street: loc[0].stateCode,
+    street: loc[0].zipcode,
+    street: loc[0].countryCode,
+  };
+
+  // Do not save address in DB
+  this.address = undefined;
+
   next();
 });
 
