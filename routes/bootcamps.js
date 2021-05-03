ca91b1c10db4448f391e11b5ea064ed08ca3da39
henrymcbean <henry_mcbean@hotmail.com>
2021-05-03 21:00:00 +0100

    9. Single Course  Add Course.

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -13,15 +13,15 @@ const courseRouter = require('./courses');
 
 const router = express.Router();
 
-// Re-route into other resource routers
+// Re-route into other resource router - will call course route '/'
 router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 
 router.route('/')
   .get(getBootcamps)
-  .post(createBootcamp);
-
+  .post(createBootcamp);      // Note even though the bootcampId is passed in url
+                              // we are still using the '/' as route for the POST
 router.route('/:id')
   .get(getBootcamp)
   .put(updateBootcamp)
