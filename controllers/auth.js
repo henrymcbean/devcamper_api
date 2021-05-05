932a90659f6b3a8e543076125f15660a83e76f9d
henrymcbean <henry_mcbean@hotmail.com>
2021-05-05 09:23:33 +0100

    2. User Register  Encrypting Passwords & bug fixes.

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,6 +7,17 @@ const User = require('../models/User');
 // @route   POST /api/v1/auth/register
 // @access  Pulic
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true });
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+
+  res.status(200).json({ success: true })
+
   next();
 });
