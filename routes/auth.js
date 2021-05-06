7aefbcbef77d09910c71ea5702b766079f5b2eb2
henrymcbean <henry_mcbean@hotmail.com>
2021-05-06 04:24:57 +0100

    7. Update User Details.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
-const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/auth');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatePassword
+} = require('../controllers/auth');
 
 const User = require('../models/User');
 
@@ -13,5 +21,7 @@ router.post('/login', login);
 router.get('/me', protect, getMe);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 
 module.exports = router;
